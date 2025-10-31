8393f516c7f48f865bf5e302fc22d87518863cf3
乃ЕTA <TA>
2025-11-01 00:36:50 +0800

    initial commit

---
+++ backend/src/modules/Auth/Auth.Login.js
@@ -0,0 +1,39 @@
+import { ConsoleLog, ConsoleError } from '../../utils/utils.logger.js';
+import Database from '../modules.connection.js';
+
+const db = new Database(true);
+const log = true;
+
+
+async function Login(req, res) {
+
+  if (!req.body) {
+    return res.status(400).json({ error: "Login Request Failed Parameter is Empty" });
+  }
+
+  try {
+
+    const collection = await db.Collection('Account');
+    const { username, password } = req.body;
+    const user = await collection.findOne({Username: username, Password: password});
+
+    if (!user) {
+      return res.status(401).json({ error: "Account not found"});
+    } 
+
+    return res.status(200).json({ error: "Login Successful" });
+
+  } catch (error) {
+    if (error.code === 11000) {
+      ConsoleError(`[ FAILED TO LOGIN ACCOUNT ]: ${error.message}`, log);
+      return res.status(409).json({ error: "Login error please try again" });
+    }
+    return res.status(500).json({ error: "Internal Server Error"});
+  } finally {
+    ConsoleLog('[ CLOSING LOGIN CONNECTION ]', log);
+    await db.Close();
+  }
+};
+
+export default Login
+
---
+++ backend/src/modules/Notification/Notification.retrieve.js
@@ -0,0 +1,13 @@
+import Database from "../modules.connection.js";
+
+const db = Database(true);
+
+async function AddNotif() {
+  try {
+    
+    
+
+  } catch (error) {
+    
+  }
+}
