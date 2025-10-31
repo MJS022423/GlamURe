21c34f22cdeece677f8c4efd633e5bbb2c8a8866
乃ЕTA <TA>
2025-11-01 00:35:15 +0800

    updated node modules and package

--- backend/src/routes/routes.status.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-
-const router = express.Router();
-
-router.get('/status', (req, res) => {
-  res.status(200).json({ status: 'ok', message: '[ EXPRESS SERVER IS RUNNING ]'});
-  console.log('[ EXPRESS SERVER IS RUNNING ]');
-});
