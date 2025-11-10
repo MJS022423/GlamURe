68d81e595e47e3b65acad0b7a7828c79bd3e24f0
乃ЕTA <TA>
2025-11-10 14:17:29 +0800

    Remove notification

--- backend/src/modules/Notification/Notification.Routes.js
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-import DisplayNotif from './Notification.Display';
-
-const Notifrouter = express.Router();
-
-Notifrouter.get('/notification', DisplayNotif);
--- backend/src/modules/Notification/Notification.message.js
+++
@@ -1,6 +0,0 @@
-const Message = {
- Added_post: (user) => {return {message: `${user} has new post`}},
- Remove_post: (user) => {return {message: `${user} remove his post`}}, 
-}
-
-export default Message;
